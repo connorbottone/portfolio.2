202d6b063c431e09ce2e3055c9de2bb2b4333e8b
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-09-02 23:30:59 -0400

    s

---
+++ portf/components/Formdata.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { send } from 'emailjs-com';
+
+const FormData = () => {
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    message: '',
+    reply_to: '',
+  });
+  const [errors, setErrors] = useState({});
+  const [success, setSuccess] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    setErrors({});
+    if (!toSend.reply_to || !/\S+@\S+\.\S+/.test(toSend.reply_to)) {
+      setErrors((prev) => ({ ...prev, reply_to: 'Please enter a valid email address' }));
+      return;
+    }
+
+    if (!toSend.message || toSend.message.length <= 10) {
+      setErrors((prev) => ({ ...prev, message: 'Message should be longer than 10 characters' }));
+      return;
+    }
+
+    send('service_v3id5r8', 'template_gy7z2dm', toSend, 'zBq2IPNkF4VkgxVPG')
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        setSuccess(true);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="container mt-1">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card p-4">
+            <h2 className="mb-4">Email Form</h2>
+            <form onSubmit={onSubmit}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="from_name"
+                  className="form-control"
+                  placeholder="Your Name"
+                  value={toSend.from_name}
+                  onChange={handleChange}
+                />
+              </div>
+              {errors.reply_to && <div className="text-danger">{errors.reply_to}</div>}
+              <div className="form-group">
+                <input
+                  type="email"
+                  name="reply_to"
+                  className="form-control"
+                  placeholder="Your Email"
+                  value={toSend.reply_to}
+                  onChange={handleChange}
+                />
+              </div>
+              {errors.message && <div className="text-danger">{errors.message}</div>}
+              <div className="form-group">
+                <textarea
+                  name="message"
+                  className="form-control"
+                  rows="5"
+                  placeholder="Your Message"
+                  value={toSend.message}
+                  onChange={handleChange}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+              {success && <div className="text-success mt-3">Email sent successfully!</div>}
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FormData;
---
+++ portf/pages/projects/index.js
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Images from '@/components/images';
+export default function Home() {
+  const [isLightMode, setIsLightMode] = useState(false);
+
+  const toggleLightMode = () => {
+    setIsLightMode(!isLightMode);
+  };
+
+  const dynamicStyles = {
+    backgroundColor: isLightMode ? '#fff' : '#000',
+    color: isLightMode ? '#000' : '#ffffff',
+  };
+
+  const cardStyles = {
+    border: "1px solid white",
+    borderRadius: "0px 104px 0px 0px",
+    padding: "20%",
+    backgroundColor: "#ffffff",
+    color: "#000",
+    marginLeft: ".7%",
+    
+    
+  };
+
+  return (
+    <>
+      <section className="vh-100" style={dynamicStyles}>
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center">
+            <div className="col col-xl-12 mb-5">
+              <div
+                style={{
+                  backgroundImage: 'url("/bbkss.gif")',
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  border: "solid white 1px",
+                  minHeight: "90vh",
+                  position: "relative",
+                }}
+                className="card"
+              >
+                <div className="row g-0">
+                  <div className="col-md-8 col-lg-9 d-flex align-items-center">
+                    <div className="card-body p-4 p-lg-5 text-black">
+                      <h3 style={{ fontWeight: "200px", fontSize: "40px" }}>
+                        <span className="text-white">Connor Bottone</span>
+                      </h3>
+                      <h3
+                        style={{
+                          fontWeight: "200px",
+                          fontSize: "17px",
+                          marginTop: "-7px",
+                          marginLeft: "4px",
+                        }}
+                        className="fw-light mb-4"
+                      >
+                        <span className="text-white">
+                          Software & Web Developer
+                        </span>
+                      </h3>
+                      <nav className="nav flex-column">
+                        <a className="nav-link text-white" href="/">
+                          Go Back
+                        </a>
+                        <a className="nav-link text-white" href="#">
+                          Contact
+                        </a>
+                        <a className="nav-link text-white" href="#">
+                          Resume
+                        </a>
+                      </nav>
+                    </div>
+                  </div>
+                </div>
+                <Images/>
+               
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
